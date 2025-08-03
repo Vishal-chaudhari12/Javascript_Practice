3c23cdd543414741fbe213b3f8feef774752d9c3
vishal_chaudhari25 <vishalchaudhari4530@gmail.com>
2025-08-03 13:55:52 +0530

    solved string prb

--- string/remove-white-spaces.js
+++ string/remove-white-spaces.js
@@ -1,12 +1,24 @@
-let str ="vi  sha l";
-removeelement ='';
+// let str ="vi  sha l";
+// removeelement ='';
 
-for(let i =0 ; i<str.length; i++){
+// for(let i =0 ; i<str.length; i++){
 
+//     if(str[i]!=' '){
+
+//         removeelement = removeelement+str[i];
+//     }
+// }
+
+// console.log(removeelement);
+
+
+let str ="vishal hellow";
+removespace ='';
+
+for(let i=0; i<str.length; i++){
     if(str[i]!=' '){
-
-        removeelement = removeelement+str[i];
+        removespace = removespace+str[i];
     }
 }
 
-console.log(removeelement);
+console.log(removespace);
---
+++ string/removedublicate.js
@@ -0,0 +1,5 @@
+const numbs =[1,1,3,4,6,6,7,9];
+
+const uniq =[...new Set(numbs)];
+
+console.log(uniq);
