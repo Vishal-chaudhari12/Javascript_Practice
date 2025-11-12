6ecb65e564f4779eb639da29e35401a0b5fb479f
vishal_chaudhari25 <vishalchaudhari4530@gmail.com>
2025-11-12 21:44:04 +0530

    array

---
+++ 1element_rotate-by-left.js
@@ -0,0 +1,19 @@
+// let arr =[1,2,3,4,5,6];
+
+// // let copy = arr[0];
+
+// // for(i=0 ; i<arr.length-1; i++){
+// //     arr[i]= arr[i+1];
+// // }
+
+// // arr[arr.length-1]= copy;
+
+// // console.log(arr);
+
+// let copy= arr[length-1]
+
+
+
+let arr =[1,2,3] +[4,5,6];
+
+console.log(arr);
